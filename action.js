0e05d07bcbf6bec3309a2659068fad4319902ad8
ziaochina <ziaochina@gmail.com>
2017-09-22 17:29:46 +0800

    zlj

--- action.js
+++ action.js
@@ -21,7 +21,7 @@ class action {
         var ret = {}
         keys.forEach(k=>{
             if(k != 'config'){
-                ret[k] = {...this.config.apps[k], _notParse: true}
+                ret[k] = {...this.config.apps[k]}
             }
         })
         return ret
